7ced0ed34c0f20373434b53eb1375d2ea77f7c11
mos2v <officialmostafa@protonmail.com>
2024-06-18 16:17:40 +0300

    .gitignore added

--- src.py
+++ src.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 img = "fronx-exterior-right-front-three-quarter-109.webp"
 model = YOLO("yolov8n.pt")
-model(img)
+results = model(source=img, show=True, conf=0.4, visualize=True)
